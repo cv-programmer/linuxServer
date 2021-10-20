28ea5c5147625765f21bd51b4cca840deaa05180
cv-programmer <usetologin@163.com>
2021-10-20 22:40:33 +0800

    进程通信之共享内存

---
+++ slide/02Linux多进程开发/08 进程间通信之共享内存/read_shm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+
+int main() {    
+
+    // 1.获取一个共享内存
+    int shmid = shmget(100, 0, IPC_CREAT);
+    printf("shmid : %d\n", shmid);
+
+    // 2.和当前进程进行关联
+    void * ptr = shmat(shmid, NULL, 0);
+
+    // 3.读数据
+    printf("%s\n", (char *)ptr);
+    
+    printf("按任意键继续\n");
+    getchar();
+
+    // 4.解除关联
+    shmdt(ptr);
+
+    // 5.删除共享内存
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
